0d0b76c78180d822cd467244a8d43754cc97bb2e
aluno-node <aluno-node@c678d.bonja.local>
2022-08-03 22:00:46 -0300

    atualizaçao

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
 const app = require('./config/server');
 
-app.listen(5000, () => {
+
+//start socket
+
+const server = app.listen(5000, () => {
     console.log('app start => http://localhost:5000');
 });
+
+
+const io = require('socket.io')(server);
+
+// setando a variael socket io para que toda a minha app possa utilizar tbm
+app.set('io', io);
+
+io.on('connection', (socket) => {
+    console.log('ENTREI NA SALA');
+
+    socket.on('disconnect', () => {
+        console.log('SAIU DA SALA');
+    });   
+
+    socket.on('msgParaServidor', (data) => {
+        socket.emit('msgParaCliente', {
+            apelido: data.apelido,
+            mensagem: data.mensagem,
+        });
+    });   
+
+    //disparando uma mensagem para todos os usuarios
+
+    socket.broadcast.emit('msgParaCliente', {
+        apelido: data.apelido,
+        mensagem: data.mensagem,
+    });
+});
+
